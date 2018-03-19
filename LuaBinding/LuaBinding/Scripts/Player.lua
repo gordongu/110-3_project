9b8bc8c8578e3ac74bf69165bdb3c7eb7d3120df
Gordon Gu <gordonsgu7@gmail.com>
2018-03-19 16:47:54 -0600

    Removed unnecessary folder

--- LuaBinding/LuaBinding/Scripts/Player.lua
+++
@@ -1,9 +0,0 @@
-local Player = {}
-
-local helloWorld = function()
-  return "Hello World"
-end
-
-Player.helloWorld = helloWorld
-
-return Player
--- LuaBinding/LuaBinding/Scripts/main.lua
+++
@@ -1,5 +0,0 @@
-local player = require("Scripts.Player")
-
-function main()
-  return player.helloWorld()
-end
