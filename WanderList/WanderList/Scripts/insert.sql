cac41754aefc8f7647fc3737629b77cb354d47b3
Gordon Gu <gordonsgu7@gmail.com>
2018-04-08 23:22:30 -0600

    Added insert.sql

---
+++ WanderList/WanderList/Scripts/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(1, "Press Play Bar Boulder", "1005 Pearl St, Boulder, CO 80302", 4, "Bar with a lot of arcade games, billards, and a dance floor");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(2, "The Walrus", "1911 11th St, Boulder, CO 80302", 4, "Bar to Dance at");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(3, "Sundown Saloon", "1136 Pearl St, Boulder, CO 80302", 3, "Bar to Play Pool at");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(4, "The Sink", "1165 13th St, Boulder, CO 80302", 3, "Bar on the Hill");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(5, "The Bitter Bar", "835 Walnut St, Boulder, CO 80302", 5, "Cocktail Bar");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(6, "Bohemian Biergarten", "2017 13th St, Boulder, CO 80302", 4, "Czech-style Bar with pretzals and karaoke on Wednesday");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(7, "The Attic Bar and Bistro", "949 Walnut St, Boulder, CO 80302", 5, "Cozy bar with games and live performance");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(8, "Dark Horse", "2922 Baseline Rd, Boulder, CO 80303", 3, "Burgers and games");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(9, "Licencse No 1", "None", 4, "None");
+
+INSERT INTO Location(location_id, name, address, rating, description)
+VALUES(10, "Tahona", "None", 4, "None");
